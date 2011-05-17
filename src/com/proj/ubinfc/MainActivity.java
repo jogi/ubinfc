e73e5995c1a3bd60b2303229912be99bde8016d5
vashishthajogi@gmail.com <vashishthajogi@gmail.com@a333fb1d-13ad-177f-ef7a-e4f6ce3eb6a8>
2011-05-17 01:35:26 +0000

    code cleanup

--- src/com/proj/ubinfc/MainActivity.java
+++ src/com/proj/ubinfc/MainActivity.java
@@ -5,6 +5,7 @@ package com.proj.ubinfc;
 
 import android.app.TabActivity;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.widget.TabHost;
 
@@ -19,26 +20,26 @@ public class MainActivity extends TabActivity {
         super.onCreate(savedInstanceState);
 
         final TabHost tabHost = getTabHost();
-        
+        Resources res = getResources();
         Intent intent = getIntent();     
         
 
-        tabHost.addTab(tabHost.newTabSpec("tab1")
+        tabHost.addTab(tabHost.newTabSpec("social")
                 .setIndicator("Social")
                 .setContent(new Intent(this, SocialActivity.class)));
 
-        tabHost.addTab(tabHost.newTabSpec("tab2")
+        tabHost.addTab(tabHost.newTabSpec("coupons")
                 .setIndicator("Coupons")
                 .setContent(new Intent(this, MainListActivity.class)));
         
         // This tab sets the intent flag so that it is recreated each time
         // the tab is clicked.
-        tabHost.addTab(tabHost.newTabSpec("tab3")
+        tabHost.addTab(tabHost.newTabSpec("products")
                 .setIndicator("Products")
                 .setContent(new Intent(this, SocialActivity.class)
                         .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)));
         
-        tabHost.addTab(tabHost.newTabSpec("tab4")
+        tabHost.addTab(tabHost.newTabSpec("authenticate")
                 .setIndicator("Authenticate")
                 .setContent(new Intent(this, SocialActivity.class)));       
         
